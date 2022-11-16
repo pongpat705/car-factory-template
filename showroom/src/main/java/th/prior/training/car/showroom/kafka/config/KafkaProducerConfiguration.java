ecf6d50ac88ddd1210a1beafde2a2297c528db02
maoz.h <pongpat.phokeed@gmail.com>
2022-11-16 11:13:39 +0700

    add security sasl configuration

--- showroom/src/main/java/th/prior/training/car/showroom/kafka/config/KafkaProducerConfiguration.java
+++ showroom/src/main/java/th/prior/training/car/showroom/kafka/config/KafkaProducerConfiguration.java
@@ -1,6 +1,9 @@
 package th.prior.training.car.showroom.kafka.config;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.config.SslConfigs;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -18,12 +21,33 @@ public class KafkaProducerConfiguration {
     @Value("${app.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
+    //security
+    @Value("${app.kafka.truststore.location}")
+    private String trustStoreLocation;
+
+    @Value("${app.kafka.truststore.password}")
+    private String trustStorePassword;
+
+    @Value("${app.kafka.security.username}")
+    private String securityUsername;
+
+    @Value("${app.kafka.security.password}")
+    private String securityPassword;
+
     @Bean
     public Map<String, Object> settingProducerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        //security
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"" + securityUsername + "\" password=\""+ securityPassword +"\";");
+        props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, trustStoreLocation);
+        props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, trustStorePassword);
+
         return props;
     }
 
