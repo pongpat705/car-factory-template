15b382b9205e446252029cb6e7ff9c46af8777ff
maoz.h <pongpat.phokeed@gmail.com>
2022-11-22 10:32:44 +0700

    fix build.sh

--- showroom/build-local.sh
+++ showroom/build-local.sh
@@ -2,6 +2,3 @@
 eval $(minikube docker-env)
 ./mvnw clean package
 docker build -t showroom:v1.0.0 .
-#then helm install credit-scoring-five-plus-listener ./helm
-#helm uninstall credit-scoring-five-plus-listener
-#kubectl get events --sort-by='.lastTimestamp'
